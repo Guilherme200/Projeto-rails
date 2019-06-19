e585ff1b48f838ba18fd1b8497a6cbb2a9e26c12
Guilherme200 <guimatoso15@gmail.com>
2019-06-18 22:13:20 -0300

    Ajustando bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :set_locale
+
+  before_action :set_locale
   def set_locale
-    session[:locale] = params[:locale] || session[:locale] || I18n.default_locale
-    I18n.locale = session[:locale]
+  session[:locale] = params[:locale] || session[:locale] || I18n.default_locale
+  I18n.locale = session[:locale]
   end
+
 
 end
